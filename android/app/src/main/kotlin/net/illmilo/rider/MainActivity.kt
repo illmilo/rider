c0de6a37f3958d2b22ac5ee8de06fa4fbf0716fb
illmilo <illfqm@gmail.com>
2025-07-07 13:48:21 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/net/illmilo/rider/MainActivity.kt
@@ -0,0 +1,5 @@
+package net.illmilo.rider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        "rider.",
+        style: TextStyle(
+          fontSize: 50,
+          fontWeight: FontWeight.w800,
+          color: Colors.white70,
+        ),
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
